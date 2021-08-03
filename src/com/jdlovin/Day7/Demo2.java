ea9a40eaf7f851a77ee2a17dbcb80a1b67295a7a
Josh Lovin <josh.lovin@revature.net>
2021-08-03 10:55:23 -0400

    Demo sess generics 1

---
+++ src/com/jdlovin/Day7/Demo2.java
@@ -0,0 +1,14 @@
+package com.jdlovin.Day7;
+
+import java.util.ArrayList;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        ArrayList<String> letters = new ArrayList<>();
+        letters.add("A");
+        letters.add("B");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+    }
+}
---
+++ src/com/jdlovin/Day7/Demo3.java
@@ -0,0 +1,28 @@
+package com.jdlovin.Day7;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+class Calculator2<T>{
+    public boolean areEqual(T a, T b){
+        if(a.equals(b))
+            return true;
+        else
+            return false;
+    }
+}
+
+public class Demo3 {
+    public static void main(String[] args) {
+        Calculator2<String> cal = new Calculator2<>();
+
+        cal.areEqual("A","B");
+
+        Calculator2<Integer> cal2 = new Calculator2<>();
+        cal2.areEqual(3,3);
+
+        Calculator2<Boolean> cal3 = new Calculator2<>();
+        cal3.areEqual(true, true);
+        
+
+    }
+}
